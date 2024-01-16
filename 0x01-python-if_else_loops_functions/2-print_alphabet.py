e7a7642d0d2dbe97846e2f246bb41d97f7377929
25-ysn <oudouchyassine@gmail.com>
2024-01-16 12:29:04 -0800

    loops

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
 for i in range(ord('a'), ord('z') + 1):
-    print(''.join(chr(i) for i in range(ord('a'), ord('z') + 1)), end='')
+    print('{:c}'.format(i), end='')
