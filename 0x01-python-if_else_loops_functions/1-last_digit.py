275e0ce9f8e819c1187a59031cc6ff81e6d0cafc
25-ysn <oudouchyassine@gmail.com>
2024-01-16 12:12:18 -0800

    loops

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-#!/usr/bin/python3
-import random
-number = random.randint(-10000, 10000)
 if number < 0:
     digitlast = number % -10
 else:
