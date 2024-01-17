98bb833fd892f876e8225ce32c28d98b956c0121
25-ysn <oudouchyassine@gmail.com>
2024-01-17 14:39:39 -0800

    loops

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for num in range(0, 99):
-    print("Decimal: {}print("Decimal: {}, Hexadecimal: {}".format(i, hex(i))))
+for i in range(99):
+    print("Decimal: {}, Hexadecimal: {}".format(i, hex(i)))
