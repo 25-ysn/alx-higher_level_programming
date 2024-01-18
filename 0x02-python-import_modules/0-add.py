f89c6ec2ea9028ed10394df91ad52f43e1e11e04
25-ysn <oudouchyassine@gmail.com>
2024-01-18 14:13:37 -0800

    modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-a = 1
-b = 2
-from add_0 import add
-result = add(a, b)
-print("{} + {} = {}".format(a, b, result))
-def add(a, b):
-    return a + b
+if __name__ == "__main__":
+    """display the sum of 1 and 2."""
+    from add_0 import add
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
