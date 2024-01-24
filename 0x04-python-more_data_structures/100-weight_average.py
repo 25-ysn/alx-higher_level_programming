be93912f7a08e8775f7c9abb3610b70d5bd0be58
25-ysn <oudouchyassine@gmail.com>
2024-01-24 09:50:33 -0800

    More Data Structures: Set, Dictionary

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    number = 0
+    val = 0
+    for x in my_list:
+        number += tup[0] * tup[1]
+        dval += tup[1]
+    return (number / val)
