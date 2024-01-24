3b2068e270ce81bdad0338815a5e9f2428d4b9f0
25-ysn <oudouchyassine@gmail.com>
2024-01-24 09:43:35 -0800

    More Data Structures: Set, Dictionary

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string: str):
+    if roman_string is None or type(roman_string) != str:
+        return 0
+    roman_numbers = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    numbers = [roman_numbers[x] for x in roman_string] + [0]
+    rep = 0
+    for j in range(len(numbers) - 1):
+        if numbers[j] >= numbers[j+1]:
+            rep += numbers[j]
+        else:
+            rep -= numbers[j]
+    return rep
