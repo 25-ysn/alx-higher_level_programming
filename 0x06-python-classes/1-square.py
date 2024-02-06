56de4609ffadeff1bf4f8f950bc58771391d7d5b
25-ysn <oudouchyassine@gmail.com>
2024-02-06 12:58:35 -0800

    python-classes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Square module."""
 
-
 class Square:
     """Defines a square."""
 
@@ -11,4 +10,3 @@ class Square:
             size: length of side of the square.
         """
         self.__size = size
-
